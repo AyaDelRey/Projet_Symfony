538884a16b5d22dc7f1d919e9a26cd88602ee6bb
Marwa Chanu <m.chanu@interface3.be>
2024-09-19 15:38:06 +0200

    ajout migration

---
+++ migrations/Version20240919121733.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240919121733 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE oeuvre ADD CONSTRAINT FK_35FE2EFEF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_35FE2EFEF675F31B ON oeuvre (author_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE oeuvre DROP FOREIGN KEY FK_35FE2EFEF675F31B');
+        $this->addSql('DROP INDEX IDX_35FE2EFEF675F31B ON oeuvre');
+    }
+}
